b7df8797dda96eccce968dc53adb06bcb74ac465
Margo Fanina <margofani.un.go@gmail.com>
2024-03-31 11:54:42 +0300

    add game rules data
    add levels and cout of answers to each

---
+++ Assets/Scripts/GameLevelData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CreateAssetMenu(fileName = "New GameLevelData", menuName = "Game Level Data", order = 10)]
+    public class GameLevelData : ScriptableObject
+    {
+        [SerializeField]
+        private int _identifier;
+        [SerializeField]
+        private int _answerVariantsCount;
+        public int Identifier => _identifier;
+        public int AnswerVariantsCount => _answerVariantsCount;
+    }
+}
---
+++ Assets/Scripts/GameRulesData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CreateAssetMenu(fileName = "New GameRulesData", menuName = "Game Rules Data", order = 10)]
+    public class GameRulesData : ScriptableObject
+    {
+        [SerializeField]
+        private GameLevelData[] _levelsData;
+
+        public GameLevelData[] GameLevelsData => _levelsData;
+    }
+}
